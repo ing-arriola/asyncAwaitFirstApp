28066f162e6cd86efd89654ed674b5a9474b0a43
ing-arriola <ingeniero.arriola@gmail.com>
2020-02-23 18:59:46 -0600

    Added a remote async await

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const getUsers= async() => {
             resolve('Data has been obtained')
         }, 3000);
     })
-
     let error=false
-
     //We could call the promise here and set then and catch but instead of it
     //we only call the promise and wait until this is acomplished
     if (!error) {
@@ -25,7 +23,28 @@ const getUsers= async() => {
 }
 //Call function and show the results of success or fail
 getUsers()
-    .then(message=>document.getElementById("app").innerHTML=message)
+    .then(message=>document.getElementById("app").innerHTML+=`<br> i'm the first promise but i need 3 seconds to be acomplished, and my result is: ${message}`)
     .catch(error=>document.getElementById("app").innerHTML=error)
 
+//Another example
+const getRemoteData = async ()=>{
+    //Get remote data
+    //Add the await to resolve the promise
+    let connnection=await fetch('https://jsonplaceholder.typicode.com/users')
+    //The connection must have another await in order to resolve its promise
+    let ans= await connnection.json()
+    return ans
+        
     
+}
+
+getRemoteData()
+    .then(ans=>{
+        ans.forEach(element => {
+            let user=document.createElement('li')
+            user.innerHTML=element.username
+            document.getElementById('app').appendChild(user)
+        })
+    }    
+    )
+    .catch( error => document.getElementById('app').innerHTML=error)
